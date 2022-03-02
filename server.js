3911385cef60ef625c7bafb60fed721ee23a324f
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-03-02 09:43:26 +0200

    Heroku setup

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
